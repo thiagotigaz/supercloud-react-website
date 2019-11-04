b9bcc7e80288dabfe6b166226883cf17191011a5
Thiago Lima <thiagotigaz@gmail.com>
2019-11-03 19:01:56 -0800

    Organize skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,17 +8,14 @@ import {
   faJava,
   faJenkins,
   faJira,
-  faLinux,
-  faNodeJs,
+  faLinux, faNodeJs,
   faPython,
+  faReact,
   faSlack,
   faWindows,
 } from "@fortawesome/free-brands-svg-icons";
 import Skill from "./Skill";
-
-const divStyle = {
-  backgroundColor: '#333',
-};
+import {faCogs, faDatabase} from "@fortawesome/free-solid-svg-icons";
 
 const Skills = (props) => {
   return (
@@ -31,21 +28,25 @@ const Skills = (props) => {
               <Skill icon={faAws} name={'AWS'} value={95}/>
               <Skill icon={faJava} name={'Java'} value={90}/>
               <Skill icon={faPython} name={'Python'} value={80}/>
-              <Skill icon={faNodeJs} name={'React'} value={65}/>
+              <Skill icon={faNodeJs} name={'NodeJs'} value={90}/>
               <Skill icon={faDocker} name={'Docker'} value={90}/>
+              <Skill icon={faCogs} name={'Microservices Patterns'} value={90}/>
             </Col>
             <Col>
+              <Skill name={'Spring Boot / Cloud'} value={90}/>
+              <Skill name={'Hibernate'} value={85}/>
+              <Skill name={'Gradle / Maven'} value={85}/>
+              <Skill name={'MySql / Postgres / RDS Aurora'} icon={faDatabase} value={90}/>
+              <Skill icon={faGithub} name={'Github'} value={100}/>
+              <Skill icon={faBootstrap} name={'Bootstrap'} value={80}/>
+            </Col>
+            <Col>
+              <Skill icon={faReact} name={'React'} value={65}/>
               <Skill icon={faJira} name={'Jira'} value={90}/>
               <Skill icon={faSlack} name={'Slack'} value={100}/>
               <Skill icon={faJenkins} name={'Jenkins'} value={80}/>
               <Skill icon={faLinux} name={'Linux'} value={100}/>
               <Skill icon={faWindows} name={'Windows'} value={65}/>
-            </Col>
-            <Col>
-              <Skill icon={faGithub} name={'Github'} value={100}/>
-              <Skill icon={faBootstrap} name={'Bootstrap'} value={80}/>
-              <Skill icon={faPython} name={'Python'} value={80}/>
-              <Skill icon={faNodeJs} name={'React'} value={65}/>
             </Col>
           </Row>
         </Container>
